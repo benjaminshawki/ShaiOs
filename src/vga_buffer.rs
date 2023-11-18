1d300ee43cd5d4aaaa5219bfbe863664faf71e28
benjaminshawki <benjaminshawki@gmail.com>
2023-11-19 00:16:50 +0100

    fix: vga_buffer working properly

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,40 +1,40 @@
 #[allow(dead_code)]
-#[derive(Debug, Clone, Copy, PartialEq Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
- Black = 0,
- Blue = 1,
- Green = 2,
- Cyan = 3,
- Red = 4,
- Magenta = 5,
- Brown = 6,
- LightGray = 7,
- DarkGray = 8,
- LightBlue = 9,
- LightGreen = 10,
- LightCyan = 11,
- LightRed = 12,
- Pink = 13,
- Yellow = 14,
- White = 15,
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
 struct ColorCode(u8);
 
 impl ColorCode {
- fn new(foreground: Color, background: Color) -> ColorCode {
-     ColorCode((background as u8) << 4 | (foreground as u8))
- }
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 struct ScreenChar {
- ascii_character: u8,
- color_code: ColorCode
+    ascii_character: u8,
+    color_code: ColorCode,
 }
 
 const BUFFER_HEIGHT: usize = 25;
@@ -42,75 +42,61 @@ const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
- chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
+
 pub struct Writer {
- column_position: usize,
- color_code: ColorCode,
- buffer: &'static mut Buffer,
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
 }
 
 impl Writer {
- pub fn write_byte(&mut self, byte: u8) {
-     match byte {
-         b'\n' => self.new_line(),
-         byte => {
-             if self.column_position >= BUFFER_WIDTH {
-                 self.newline();
-             }
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
 
-             let row = BUFFER_HEIGHT - 1;
-             let col = self.column_position;
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
 
-             let color_code = self.color_code;
-             self.buffer.chars[row][col] = ScreenChar {
-                 ascii_character: byte,
-                 color_code,
-             };
-             self.column_position += 1;
-         }
-     }
- }
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+    fn new_line(&mut self) { /* TODO */ }
 
- fn new_line(&mut self) { /* TODO */ }
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
 
- pub fn write_string(&mut self, s: &str) {
-     for byte in s.bytes() {
-         match byte {
-             // printable ASCII byte or newline
-             0x20..=0x7e | b'\n' => self.write_byte(byte),
-             // not part of printable ASCII range
-             _ => self.write_byte(0xfe),
-         }
-     }
- }
+        }
+    }
 }
+
 
 
 pub fn print_something() {
- let mut writer = Writer {
-     column_position: 0,
-     color_code: ColorCode::new(Color::Yellow, Color::Black),
-     buffer: unsafe { &mut *(0xb8000 as *mut Buffer)},
- };
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
 
- writer.write_byte(b'H');
- writer.write_string("ello ");
- writer.write_string("Wörld!")
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    writer.write_string("Wörld!");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
