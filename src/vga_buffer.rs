0c2613abaf79cc18461a4693f31cd48ac27d4745
Benjamin Shawki <benjaminshawki@gmail.com>
2023-11-19 03:26:43 +0100

    static

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,9 @@
+use core::fmt::{Write, Result};
+
+use volatile::Volatile;
+
+use lazy_static::lazy_static;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
@@ -36,11 +42,9 @@ struct ScreenChar {
     ascii_character: u8,
     color_code: ColorCode,
 }
-
 const BUFFER_HEIGHT: usize = 25;
-const BUFFER_WIDTH: usize = 80;
 
-use volatile::Volatile;
+const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
 struct Buffer {
@@ -51,6 +55,21 @@ pub struct Writer {
     column_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
+}
+
+lazy_static! {
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+}
+
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result {
+        self.write_string(s);
+        Ok(())
+    }
 }
 
 impl Writer {
@@ -75,7 +94,25 @@ impl Writer {
         }
     }
     fn new_line(&mut self) {
-        todo!();
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row-1][col].write(character);
+
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character:b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
     }
 
     pub fn write_string(&mut self, s: &str) {
@@ -102,5 +139,5 @@ pub fn print_something() {
 
     writer.write_byte(b'H');
     writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
